46c2401bfeb34e11fdb276cedd11ce184b00aec9
nakitamccool <nakita@codeinstitute.net>
2017-09-11 10:52:42 +0000

    Created UserProfile and provided admin access to create new users
    Created new UserProfile model
    Created login, profile, logout views and forms
    Created login, profile, logout, register urls
    Updated text on index.html
    Created superuser (ubuntu)

--- custom_authentication/accounts/urls.py
+++ custom_authentication/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from .views import get_index
+from .views import get_index, register, profile, logout, login
 
 
 urlpatterns = [
-    url(r'^$', get_index, name='get_index')
+    url(r'^$', get_index, name='get_index'),
+    url(r'^register$', register, name='register'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^logout$', logout, name='logout'),
+    url(r'^login$', login, name='login'),
 ]
